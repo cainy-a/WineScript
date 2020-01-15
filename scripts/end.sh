a07d3eb16f8c830b007c632e64c28eb76663dd36
cainy-a <cainatkinson98@gmail.com>
2020-01-15 18:37:21 +0000

    Added link to Github Issues

--- scripts/end.sh
+++ scripts/end.sh
@@ -2,5 +2,5 @@ echo 'The install of Wine is complete, please read the following important infor
 echo ''
 echo 'When you run the first exe with wine, it will request the installation of wine-mono and then wine-gecko. You MUST, I repeat, you MUST install these when prompted.'
 echo ''
-echo 'Apart from that, thanks for using my installer, and happy wining!'
+echo 'Apart from that, thanks for using my installer, report any issues at https://github.com/cainy-a/WineScript/issues, and happy wining!'
 exit
