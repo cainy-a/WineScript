6a27bde4d6ba2bdb980c21347a8e72566d07c000
cainy-a <cainatkinson98@gmail.com>
2020-01-15 10:49:10 +0000

    skeleton version branching

--- main.sh
+++ main.sh
@@ -1,1 +1,17 @@
-echo Hello World
+echo 'Welcome to the Ubuntu Wine Auto Installer by Cain Atkinson,'
+echo 'Please select your Ubuntu Version.'
+read -s 'x=Xenial, b=Bionic, d=Disco, e=Eoan: ' version
+if [$version == "x"];
+then
+  ./Xenial.sh
+  exit
+if [$version == "b"]
+then
+  ./Bionic.sh
+  exit
+if [$version == "d"]
+then
+  ./Disco.sh
+  exit
+if [$version == "e"]
+t
